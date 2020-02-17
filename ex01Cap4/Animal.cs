d6d0e243e640ca0f5f593a3b5a1d831d749be48d
staicu adrian <adrian_staicu88@yahoo.com>
2020-02-17 21:27:37 +0200

    Add project files.

---
+++ ex01Cap4/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01Cap4
+{
+    class Animal
+    {
+
+        public string tipAnimal { get; set; }
+        public string culoare { get; set; }
+        public int varsta { get; set; }
+
+        public string detaliiAnimal()
+        {
+            return "tipAnimal: " + tipAnimal + "\nCuloare: " + culoare + "\nVarsta: " + varsta;
+        }
+
+        //constructor implicit
+        public Animal()
+        {
+            tipAnimal = "";
+            culoare = "";
+            varsta = 0;
+        }
+
+
+
+        //constructor cu 2 parametri
+        public Animal(string tipAnimal, string culoare)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = culoare;
+            varsta = 0;
+        }
+
+        //constructor cu 3 parametri
+        public Animal(string tipAnimal, string culoare, int varsta)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = culoare;
+            this.varsta = varsta;
+        }
+   
+    }
+}
---
+++ ex01Cap4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creati un obiect Animal cu caracteristicile: tipAnimal de tip String (caine, pisica, iepure etc), culoare de tip String, varsta de tip int. 
+            //Clasa va contine declararea variabilelor, metode de tip accesor (set si get) si 3 constructori.
+            //In Program.cs sau o clasa cu metoda Main creati 3 instante ale clasei Animal in metoda main.
+
+            Animal x1 = new Animal();
+            x1.detaliiAnimal();
+            x1.tipAnimal = "catel";
+            x1.culoare = "alb";
+            x1.varsta = 10;
+
+            Console.WriteLine();
+            Console.WriteLine("TipAnimal: " + x1.tipAnimal + "\nCuloare: " + x1.culoare + "\nVarsta: " + x1.varsta);
+
+            Animal x2 = new Animal("pisica", "neagra");
+            x2.varsta = 4;
+
+            Console.WriteLine();
+            Console.WriteLine("TipAnimal: " + x2.tipAnimal + "\nCuloare: " + x2.culoare + "\nVarsta: " + x2.varsta);
+
+
+            Animal x3 = new Animal("papagal", "albastru", 3);
+            Console.WriteLine();
+            Console.WriteLine("TipAnimal: " + x3.tipAnimal + "\nCuloare: " + x3.culoare + "\nVarsta: " + x3.varsta);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
